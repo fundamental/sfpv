35a90ebe5c56456e0c2278bbb9f48a373d2ada4a
fundamental <mark.d.mccurry@gmail.com>
2012-05-21 09:30:11 -0400

    Fixing error message capitalization

--- src/Errors.cpp
+++ src/Errors.cpp
@@ -11,15 +11,15 @@ void errors_init(clang::DiagnosticsEngine &e)
 {
     error_realtime_saftey_violation =
         e.getCustomDiagID(clang::DiagnosticsEngine::Error,
-                "Realtime saftey violation, %0 Called from a safe function");
+                "Realtime saftey violation, %0 called from a safe function");
 
     error_realtime_saftey_trace =
         e.getCustomDiagID(clang::DiagnosticsEngine::Note,
-                "%0 Deduced Realtime safe");
+                "%0 deduced realtime safe");
 
     error_realtime_saftey_trace_end =
         e.getCustomDiagID(clang::DiagnosticsEngine::Note,
-                "%0 Specified Realtime safe by user");
+                "%0 specified realtime safe by user");
 
     warnn_realtime_saftey_unknown =
         e.getCustomDiagID(clang::DiagnosticsEngine::Warning,
